8616ee0d576f7c2cf1cc0e795d5cd8cc52065589
sahilgadekar1507 <sahilgadekar15@gmail.com>
2026-03-25 12:38:08 +0530

    basic working connection has build

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-import express from "express";
-import { createServer } from "node:http";
-import { fileURLToPath } from "node:url";
-import { dirname, join } from "node:path";
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
-const srver = createServer(app);
+const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
-    res.sendFile(join(__dirname , "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log("server is listening on the port http://localhost:3000"); 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//   });
+// });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
 });
